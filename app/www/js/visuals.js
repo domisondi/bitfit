9da40c29582a5333392bfde79b5c3019dc85be25
Notebook\Erik <spuny.el@gmail.com>
2016-09-17 14:09:30 +0200

    no message

--- app/www/js/visuals.js
+++ app/www/js/visuals.js
@@ -13,6 +13,6 @@ $('a.collection').height(.5 * $('a.collection').width());
 
 var colors = ['#c73b0b', '#e7a136', '#978e43', '#9b2f10', '#731115'];
 $('a.collection').each(function(i) {
-	$(this).prepend('#' + i+)
+	$(this).prepend('#' + i++);
 	$(this).css('background-color', colors[i]);
 })
