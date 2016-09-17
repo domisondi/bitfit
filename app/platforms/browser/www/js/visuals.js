988e52b8ab302f3e596b06cb3c72a044a75a7f35
Robin Bisping <robin@bisping.ch>
2016-09-17 13:55:22 +0200

    UI Stuff

--- app/platforms/browser/www/js/visuals.js
+++ app/platforms/browser/www/js/visuals.js
@@ -1,18 +1,15 @@
-
-$(function() {
-    $('.center').css({
-        'position' : 'absolute',
-        'left' : '50%',
-        'top' : '50%',
-        'margin-left' : function() {return -$(this).outerWidth()/2},
-        'margin-top' : function() {return -$(this).outerHeight()/2}
-    });
+$('.center').css({
+    'position' : 'absolute',
+    'left' : '50%',
+    'top' : '50%',
+    'margin-left' : function() {return -$(this).outerWidth()/2},
+    'margin-top' : function() {return -$(this).outerHeight()/2}
 });
 
 $('a.collection').height(.5 * $('a.collection').width());
 
-var colors = ['#c73b0b', '#e7a136', '#978e43', '#9b2f10', '#731115'];
+var colors = ['#00a0b0', '#cc333f', '#eb6841', '#edc951'];
 $('a.collection').each(function(i) {
-	$(this).prepend('#' + i+)
-	$(this).css('background-color', colors[i]);
-})
+	$(this).prepend('<span>#' + (i+1).toString() + '</span>');
+	$(this).css('background-color', colors[i % colors.length]);
+});
