e8f7f7f8057ccbcc1eff9cd083294d1533d70036
Dominic Sonderegger <dominic.sonderegger@hispeed.ch>
2016-09-17 13:54:14 +0200

    completion of items, step count etc.

--- server/api/stats.php
+++ server/api/stats.php
@@ -5,11 +5,7 @@ if(isset($_REQUEST['access_token']) && isset($_REQUEST['user_id'])){
     global $user;
     $user = new User($_REQUEST['user_id']);
     $user->set_access_token($_REQUEST['access_token']);
-    $output['bits'] = $user->refresh_bits_count();
     
-    function action_after_api() {
-        global $user;
-        $user->update_in_database();
-    }
+    $output['stats'] = $user->get_stats();
 }
 else throw new Exception('Unrecognized request.');
--- server/core/User.php
+++ server/core/User.php
@@ -5,6 +5,7 @@ class User {
     var $id;
     var $name;
     var $bits;
+    var $steps_used;
     
     private $fitbit;
     private $access_token;
@@ -29,23 +30,29 @@ class User {
         else {
             $this->name = $id;
             $this->bits = 0;
-            
-            $database->query("INSERT INTO users (id, name, bits) VALUES (:id, :name, :bits)");
-            $database->bind('id', $id);
-            $database->bind('name', $this->name);
-            $database->bind('bits', $this->bits);
-            $database->execute();
+            $this->steps_used = $this->get_fitbit_lifetime_stepcount();
+            $this->insert_into_database();
         }
         
         $this->fitbit = new FitBit();
     }
     
-    public function update_in_database() {
+    public function insert_into_database() {
         global $database;
-        $database->query("UPDATE users SET name=:name, bits=:bits WHERE id=:id");
+        $database->query("INSERT INTO users (id, name, bits) VALUES (:id, :name, :bits)");
         $database->bind('id', $this->id);
         $database->bind('name', $this->name);
         $database->bind('bits', $this->bits);
+        $database->execute();
+    }
+    
+    public function update_in_database() {
+        global $database;
+        $database->query("UPDATE users SET name=:name, bits=:bits, steps_used=:steps_used WHERE id=:id");
+        $database->bind('id', $this->id);
+        $database->bind('name', $this->name);
+        $database->bind('bits', $this->bits);
+        $database->bind('steps_used', $this->steps_used);
         $database->execute();
     }
     
@@ -68,5 +75,37 @@ class User {
     
     public function has_item_completed($item){
         return $item->is_done_by_user($this);
+    }
+    
+    public function get_current_step_count() {
+        return $this->get_fitbit_lifetime_stepcount() - $this->steps_used;
+    }
+    
+    public function get_fitbit_lifetime_stepcount() {
+        $this->fitbit->init($this->id, $this->get_access_token());
+        $raw_data = $this->fitbit->get_data('user/[user-id]/activities.json');
+        $data = json_decode($raw_data);
+        
+        $stepcount = $data->lifetime->total->steps;
+        
+        return $stepcount;
+    }
+    
+    public function get_stats() {
+        $stats = array();
+        $stats['step_count'] = $this->get_current_step_count();
+        
+        return $stats;
+    }
+    
+    public function complete_item($item){
+        $this->steps_used += $item->nr_steps;
+        $this->update_in_database();
+        
+        global $database;
+        $database->query("INSERT INTO items_done (user_id, coll_id, item_id) VALUES (:user_id, :coll_id, :item_id)");
+        $database->bind('user_id', $this->id);
+        $database->bind('coll_id', $item->coll_id);
+        $database->bind('item_id', $item->id);
     }
 }
