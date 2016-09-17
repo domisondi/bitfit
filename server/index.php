c76f4585c159ebdb2649460afae17bc3f6312b29
Dominic Sonderegger <dominic.sonderegger@hispeed.ch>
2016-09-17 04:35:20 +0200

    ..

--- server/index.php
+++ server/index.php
@@ -1,12 +1,85 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        
+        <link rel="stylesheet" href="css/style.css">
+        
         <meta charset="UTF-8">
-        <title></title>
+        <title>BitFit Store</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div id="main-wrapper">
+            <div id="nav-wrapper" class="navbar navbar-default" role="navigation">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button id="navbar-collapse-button" type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <h3 id="title"><a href=""><img id="logo" height="55" src="" alt="<?php echo 'BitFit Store'; ?>"></a><?php echo 'BitFit Store'; ?></h3>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        <ul id="navbar-content" class="nav navbar-nav navbar-right">
+                            <li><a href=""><?php echo 'Home'; ?></a></li>       
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
+            <br>
+            <div id="main-container-wrapper" class="container">
+                <div id="main-container" class="container" style="padding-left:0;">
+                    <div id="head" class="row">
+                        <div class="col-sm-12">
+
+                        </div>
+                    </div>
+                    <div id="main-content-container" class="row">
+                        <div id="content-wrapper" class="col-sm-12">
+                            <?php switch($_GET['page']){
+                                default: ?>
+                            
+                            <div class="container" id="homepage-form">
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <form action="" method="POST">
+                                            <div class="form-group">
+                                                <label>Item-Name<input type="text" class="form-control" name="item_name" value="" placeholder="Dein Item-Name..."></label>
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Anzahl Schritte<input type="number" class="form-control" name="nr_steps" value="" placeholder="100000"></label>
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="submit" class="btn btn-primary" name="create_item" value="Erstellen">
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                                    <?php break;
+                            } ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style="height:45px;"></div>
+            <div id="footer" class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+                <div class="container">
+                    <div class="navbar-text">
+                        <p class="text-center">
+                            <?php echo 'Copyright &copy; 2016 – BitFit'; ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
